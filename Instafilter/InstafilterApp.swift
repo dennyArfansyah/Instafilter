228d7da32b4812de3de9f624322606c377e011f9
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-29 17:30:44 +0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Denny Arfansyah on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
